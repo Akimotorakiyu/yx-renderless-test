b3139bf25eff62001b7d582d7686a47551227062
湫曗 <autumnsilverleaves@gmail.com>
2021-01-31 18:57:31 +0800

    作用域

--- log.ts
+++ log.ts
@@ -1,7 +1,14 @@
+// 局部作用域
 for (let index = 0; index < 10; index++) {
   setTimeout(() => {
     console.log(index);
   }, 1);
 }
 
-
+// 全局作用域
+let index = 0;
+for (; index < 10; index++) {
+  setTimeout(() => {
+    console.log(index);
+  }, 1);
+}
