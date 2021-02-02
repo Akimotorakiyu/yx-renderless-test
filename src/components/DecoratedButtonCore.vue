7e14cc6e6d7cdd90eb1175d16f85797c5f9e738c
湫曗 <autumnsilverleaves@gmail.com>
2021-02-02 17:37:38 +0800

    add DecoratedButtonCore

---
+++ src/components/DecoratedButtonCore.vue
@@ -0,0 +1,29 @@
+<template>
+  <PlainButton>
+    <slot>按钮</slot>
+  </PlainButton>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent, reactive, provide, PropType } from "vue";
+import { ButtonCore } from "./buttonCore";
+import PlainButton from "./PlainButton.vue";
+export default defineComponent({
+  name: "ButtonCore",
+  components: {
+    PlainButton,
+  },
+  props: {
+    logic: {
+      type: Function as PropType<(event: Event) => void>,
+      require: true,
+    },
+  },
+  setup(props) {
+    // const { runner, status } = useButtonCore(props);
+    const buttonCore = new ButtonCore(props);
+
+    return { buttonCore };
+  },
+});
+</script>
--- src/views/comUse/ComUse.vue
+++ src/views/comUse/ComUse.vue
@@ -1,35 +1,48 @@
 <template>
   <div>
+    <div>
+      <h2>直接使用</h2>
+      <DecoratedButtonCore :logic="run"> 普通 </DecoratedButtonCore>
+      <DecoratedButtonCore :logic="run" class="btn-primary">
+        主要
+      </DecoratedButtonCore>
+      <DecoratedButtonCore :logic="run" class="btn-success">
+        成功
+      </DecoratedButtonCore>
+      <DecoratedButtonCore :logic="run" class="btn-info">
+        信息
+      </DecoratedButtonCore>
+      <DecoratedButtonCore :logic="run" class="btn-warning">
+        警告
+      </DecoratedButtonCore>
+      <DecoratedButtonCore :logic="run" class="btn-error">
+        危险
+      </DecoratedButtonCore>
+    </div>
+    <h2>自定义使用</h2>
     <ButtonCore :logic="run">
       <template #default="{ runner, status }">
-        <button
-          class="border rounded-lg px-2 py-1 m-1"
-          @click="runner"
-          :disabled="status.pending"
-        >
+        <button class="btn" @click="runner" :disabled="status.pending">
           000
         </button>
       </template>
     </ButtonCore>
     <ButtonCore :logic="run">
       <template #default="{ runner, status }">
-        <plain-button
-          class="border rounded-lg px-2 py-1 m-1"
-          :logic="runner"
-          :status="status"
+        <plain-button class="btn" :logic="runner" :status="status"
           >111</plain-button
         >
       </template>
     </ButtonCore>
     <ButtonCore :logic="run">
-      <plain-button class="border rounded-lg px-2 py-1 m-1"
+      <plain-button class="btn"
         ><div>111</div>
         <div>222</div></plain-button
       >
     </ButtonCore>
   </div>
   <div>
-    <h2>带样式使用</h2>
+    <h2>注入使用</h2>
     <ButtonCore :logic="run">
       <plain-button class="">普通</plain-button>
     </ButtonCore>
@@ -55,12 +68,14 @@
 import { defineComponent } from "vue";
 import ButtonCore from "../../components/ButtonCore.vue";
 import PlainButton from "../../components/PlainButton.vue";
+import DecoratedButtonCore from "../../components/DecoratedButtonCore.vue";
 
 export default defineComponent({
   name: "ComUse",
   components: {
     ButtonCore,
     PlainButton,
+    DecoratedButtonCore,
   },
   setup() {
     function run() {
